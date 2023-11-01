cfef842256217c6e69017594f137ecdcc80654c6
dhrutisubham <dhruti.subham@gmail.com>
2023-11-01 18:47:49 +0530

    updated route for user login and session

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,31 +1,29 @@
-import '@styles/globals.css';
-import Nav from '@components/Nav';
-import Provider from '@components/Provider';
+import "@styles/globals.css";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
-export const metadata={
-        title:"PromptOverFlow",
-        description:"Prompting and Sharing AI Prompts"
-    
-}
+export const metadata = {
+  title: "PromptOverFlow",
+  description: "Prompting and Sharing AI Prompts",
+};
 
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-          <body>
-              <div className='main'>
-                  <div className='gradient' />
-              </div>
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
 
-              <main className='app'>
-                <Nav/>
-                {children}
-
-              </main>
-          </body>
-
-      </html>
-      
-  )
-}
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
+    </html>
+  );
+};
 
 export default RootLayout;
